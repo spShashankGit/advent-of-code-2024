c147ea5dbfda6e136c3dec911fd23d3e7508d0b9
spShashankGit <shashank_pandey93@yahoo.com>
2024-12-08 16:48:00 +0100

    Add the potential approach

--- scripts/advent-day-8/main.py
+++ scripts/advent-day-8/main.py
@@ -10,8 +10,6 @@ Aim:
 1. Find out how many unique antenna positions could be there.
 1.1 To do we need to find out how many of the 
 
-
-
 Concepts important for problem solving:
 1. Antinodes: An antinote occurs at a point that is perfectly in line with the two antennas of the same frequeny
 - but only when one of the antennas is twice as far away as the other. 
@@ -39,7 +37,15 @@ print("Current Working Directory:", os.getcwd())
 print('Advent of Code 2024 - Day 8')
 filec = '../../inputs/advent-day-8/input-set-1.txt'
 
+"""
+# Defining the approach
+1. Need to the node i.e. either a-z, A-Z or 0-9
+2. Need to identify the poisition of the another node inside the inputs stiing
+3. Idenfiy the distance between the two nodes
+4. Identify the potential antinode position(s) based on the distance between the two nodes and also the positiong inside 
+"""
+
 with open(filec, 'r') as file:
     for line in file:
         print(line.strip())  # strip() removes leading/trailing whitespace
-        line.find()
+        #line.find()
