7a58d182b45c09c297cb8e9cd86c93f4303482c0
spShashankGit <shashank_pandey93@yahoo.com>
2024-12-08 16:22:43 +0100

    Add the code to read the input line by line

--- scripts/advent-day-8/main.py
+++ scripts/advent-day-8/main.py
@@ -6,6 +6,11 @@ Input text is a text with following contstraints:
 4. In the example antinodes are represetned by # and 0.
 
 
+Aim:
+1. Find out how many unique antenna positions could be there.
+1.1 To do we need to find out how many of the 
+
+
 
 Concepts important for problem solving:
 1. Antinodes: An antinote occurs at a point that is perfectly in line with the two antennas of the same frequeny
@@ -26,9 +31,15 @@ example: # a a #
 
 Reference:
 1. Online:  https://adventofcode.com/2024/day/8
-2. Offline: ../../questions/advent-of-code-2024/day-8.md
+2. Offline: ../questions/advent-of-code-2024/day-8.md
 
 """
-
-
+import os
+print("Current Working Directory:", os.getcwd())
 print('Advent of Code 2024 - Day 8')
+filec = '../../inputs/advent-day-8/input-set-1.txt'
+
+with open(filec, 'r') as file:
+    for line in file:
+        print(line.strip())  # strip() removes leading/trailing whitespace
+        line.find()
